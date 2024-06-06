528bc404eb4db25d136b6aec7f0ced9eb5c54a7e
Peter Kok <kretep@users.noreply.github.com>
2024-06-06 11:35:23 +0200

    Dummy test

--- src/paradigma/__init__.py
+++ src/paradigma/__init__.py
@@ -1,1 +1,8 @@
-pass
+# read version from installed package
+from importlib.metadata import version
+
+__version__ = version("paradigma")
+
+from .dummy import (
+    hello_world
+)
---
+++ tests/test_paradigma.py
@@ -0,0 +1,6 @@
+# Dummy test
+import paradigma
+
+def test_dummy():
+    paradigma.hello_world() == "Hello, world!"
+
